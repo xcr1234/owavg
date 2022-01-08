caf85a045a2f2baec84656cbb363af3c076d99d4
xcr1234 <530551426@qq.com>
2022-01-08 14:41:37 +0800

    添加百度统计

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports ={
   lintOnSave: false,
   publicPath:  process.env.NODE_ENV === 'production'
-    ? '/owavg-tool/' // production base url.
+    ? '/owavg/' // production base url.
     : '/',
 }
